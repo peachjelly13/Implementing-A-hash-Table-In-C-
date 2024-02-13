190a2237508df3016704e9a1e9753a652e52fcd6
peachjelly13 <ananya.sharma2021a@vitstudent.ac.in>
2024-02-13 23:52:23 +0530

    hash_table design and item add

---
+++ src/hash_table.h
@@ -0,0 +1,19 @@
+//This represents one item in my hash table there is an item and there is a value related to that item
+typedef struct{
+    char* key;
+    char* value;
+}hash_table_item;
+
+//An array of items will be stored it is stored as pointers to hash_table_item , we store 
+//multiple items in the hash table like this
+//array of pointers to hash_table_items
+//we also keep a track of the size of the hash table and how many items have been added
+
+typedef struct{
+    int size;
+    int count;
+    hash_table_item** item; //represents an array of pointers
+}hash_table_design;
+
+
+
